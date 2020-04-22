08cc2dba35791fb39e9b8bd86686508b9e9c01ee
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2020-04-21 17:54:33 -0700

    OpenAL build rule and example.

---
+++ example/win32/openal.cc
@@ -0,0 +1,39 @@
+#include <cstdio>
+
+#include <windows.h>
+
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+
+#include <cstdio>
+#include <string.h>
+
+void
+ListAudioDevices(const ALCchar* devices)
+{
+  const ALCchar* device = devices, *next = devices + 1;
+  size_t len = 0;
+
+  printf("Device List\n");
+  while (device && *device != '\0' && next && *next != '\0') {
+    printf("%s\n", device);
+    len = strlen(device);
+    device += (len + 1);
+    next += (len + 2);
+  }
+}
+
+int
+main()
+{
+  ALCdevice* device;
+  device = alcOpenDevice(nullptr);
+  if (!device) printf("Could not open device.\n");
+  ALboolean enumeration;
+  enumeration = alcIsExtensionPresent(nullptr, "ALC_ENUMERATION_EXT");
+  if (enumeration == AL_FALSE) printf("ALC_ENUMERATION_EXT not present\n");
+  ListAudioDevices(alcGetString(nullptr, ALC_DEVICE_SPECIFIER));
+  ALCenum error = alGetError();
+  if (error != AL_NO_ERROR) printf("openal error\n");
+  return 0;
+}

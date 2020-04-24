3c2c119ccbec424e1fcc1b2b65337c3a625cdae8
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2020-04-24 08:04:56 -0700

    Example now plays two sounds at the same time.

--- example/audio/wav_player.cc
+++ example/audio/wav_player.cc
@@ -13,12 +13,18 @@ main()
   audio::Initialize();
   audio::SetListener(v3f(0.f, 0.f, 1.f), v3f(0.f, 0.f, 0.f),
                      v3f(0.f, 0.f, 1.f), v3f(0.f, 1.f, 0.f));
-  uint32_t aid = audio::LoadSound("example/audio/test.wav");
-  if (!aid) {
+  uint32_t aid1 = audio::LoadSound("example/audio/test.wav");
+  if (!aid1) {
     printf("Unable to load sound...\n");
     return 1;
   }
-  audio::PlaySound(aid);
-  platform::sleep_ms((uint64_t)audio::FindSound(aid)->length_ms);
+  uint32_t aid2 = audio::LoadSound("example/audio/step.wav");
+  if (!aid2) {
+    printf("Unable to load sound...\n");
+    return 1;
+  }
+  audio::PlaySound(aid1);
+  audio::PlaySound(aid2);
+  platform::sleep_ms((uint64_t)audio::FindSound(aid1)->length_ms);
   return 0;
 }
